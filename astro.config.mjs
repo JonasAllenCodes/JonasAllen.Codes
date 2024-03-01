ec4300a5a9d72221b4aa293128f92af762ebe43c
Jonas Allen <Webmaster@JonasAllen.Codes>
2024-02-29 19:15:27 -0800

    FIX Fix structure and styles
    Turned more of the layout into components and fixed some styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [
+    icon({
+      include: {
+        "fa6-brands": ["facebook", "instagram", "x-twitter"],
+      },
+    }),
+  ],
 });
